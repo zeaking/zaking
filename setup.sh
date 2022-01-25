74c88cb4a56970446ed9bc8031ee0894b550f013
zeaking <85456530+zeaking@users.noreply.github.com>
2022-01-26 00:20:07 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -143,20 +143,20 @@ echo "   - OHP Dropbear            : 8282"  | tee -a log-install.txt
 echo "   - OHP OpenVPN             : 8383"  | tee -a log-install.txt
 echo "   - Tr Go                   : 2053"  | tee -a log-install.txt
 echo ""| tee -a log-install.txt
-echo "TServer Information & Other Features"  | tee -a log-install.txt
-echo "Timezone                : Asia/Jakarta (GMT +7)"  | tee -a log-install.txt
-echo "Fail2Ban                : [ON]"  | tee -a log-install.txt
-echo "- Dflate                  : [ON]"  | tee -a log-install.txt
-echo "- IPtables                : [ON]"  | tee -a log-install.txt
-echo "- Auto-Reboot             : [ON]"  | tee -a log-install.txt
-echo "- IPv6                    : [OFF]"  | tee -a log-install.txt
-echo "- Autoreboot On 05.00 GMT +7" | tee -a log-install.txt
-echo "- Autobackup Data" | tee -a log-install.txt
-echo "- Restore Data" | tee -a log-install.txt
-echo "- Auto Delete Expired Account" | tee -a log-install.txt
-echo "- Full Orders For Various Services" | tee -a log-install.txt
-echo "- White Label" | tee -a log-install.txt
-echo "- Installation Log --> /root/log-install.txt"  | tee -a log-install.txt
+echo "Server Information & Other Features"  | tee -a log-install.txt
+echo "   - Timezone                : Asia/Jakarta (GMT +7)"  | tee -a log-install.txt
+echo "   - Fail2Ban                : [ON]"  | tee -a log-install.txt
+echo "   - Dflate                  : [ON]"  | tee -a log-install.txt
+echo "   - IPtables                : [ON]"  | tee -a log-install.txt
+echo "   - Auto-Reboot             : [ON]"  | tee -a log-install.txt
+echo "   - IPv6                    : [OFF]"  | tee -a log-install.txt
+echo "   - Autoreboot On 05.00 GMT +7" | tee -a log-install.txt
+echo "   - Autobackup Data" | tee -a log-install.txt
+echo "   - Restore Data" | tee -a log-install.txt
+echo "   - Auto Delete Expired Account" | tee -a log-install.txt
+echo "   - Full Orders For Various Services" | tee -a log-install.txt
+echo "   - White Label" | tee -a log-install.txt
+echo "   - Installation Log --> /root/log-install.txt"  | tee -a log-install.txt
 echo ""  | tee -a log-install.txt
 echo "   - Dev/Main                : Horas Marolop Amsal Siregar"  | tee -a log-install.txt
 echo "   - Recode                  : geovpn" | tee -a log-install.txt
