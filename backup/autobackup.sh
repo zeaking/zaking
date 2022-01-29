0d0b8f0f565cf08654b581633b844eac0f4442e1
zeaking <85456530+zeaking@users.noreply.github.com>
2022-01-29 21:53:21 +0700

    Update autobackup.sh

--- backup/autobackup.sh
+++ backup/autobackup.sh
@@ -14,13 +14,13 @@ LIGHT='\033[0;37m'
 # Getting
 MYIP=$(wget -qO- ipinfo.io/ip);
 echo "Checking VPS"
-IZIN=$( curl https://raw.githubusercontent.com/geovpn/perizinan/main/ip | grep $MYIP )
+IZIN=$( curl https://raw.githubusercontent.com/zeaking/izin/main/ip.txt | grep $MYIP )
 if [ $MYIP = $IZIN ]; then
 echo -e "${NC}${GREEN}Permission Accepted...${NC}"
 else
 echo -e "${NC}${RED}Permission Denied!${NC}";
 echo -e "${NC}${LIGHT}Please Contact Admin!!"
-echo -e "${NC}${LIGHT}Telegram : https://t.me/geovpn"
+echo -e "${NC}${LIGHT}Telegram : https://t.me/zeaking5"
 exit 0
 fi
 clear
